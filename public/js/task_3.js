e8357a48ef404ebf9fe08210d6d034cae604d7c1
Adil Rabbani <adil_rabbani@ymail.com>
2017-12-01 22:37:53 +0500

    Task 3 completed

--- public/js/task_3.js
+++ public/js/task_3.js
@@ -1,12 +1,14 @@
 function task3(e){
     var mousePosition = new Cesium.Cartesian2(e.clientX, e.clientY);
  
+    var info_box = document.getElementById("info");
+
     var ellipsoid = viewer.scene.globe.ellipsoid;
     var cartesian = viewer.camera.pickEllipsoid(mousePosition, ellipsoid);
     if (cartesian) {
         var cartographic = ellipsoid.cartesianToCartographic(cartesian);
-        var longitudeString = Cesium.Math.toDegrees(cartographic.longitude).toFixed(2);
-        var latitudeString = Cesium.Math.toDegrees(cartographic.latitude).toFixed(2);
+        var longitudeString = Cesium.Math.toDegrees(cartographic.longitude).toFixed(5);
+        var latitudeString = Cesium.Math.toDegrees(cartographic.latitude).toFixed(5);
        
         var pointOfInterest = Cesium.Cartographic.fromDegrees(
         parseFloat(longitudeString), parseFloat(latitudeString), 5000, new Cesium.Cartographic());
@@ -19,6 +21,13 @@ function task3(e){
         var promise = Cesium.sampleTerrain(terrainProvider, 11, positions);
         Cesium.when(promise, function(updatedPositions) {
             console.log(longitudeString+','+latitudeString+','+positions[0].height);
+            info_box.style.display = "block";
+            console.log(e.pageX);
+            console.log(e.pageY);
+            info_box.style.left = e.pageX;
+            info_box.style.top  = e.pageY;
+            info_box.innerHTML = "Longitude : " + longitudeString + "<br>" + "Latitude :<br>" + latitudeString + "<br>" + "Altitude :<br>" + positions[0].height; 
+            
             
             // updatedPositions is just a reference to positions.
         });
--- public/main.js
+++ public/main.js
@@ -1,5 +1,15 @@
 Cesium.BingMapsApi.defaultKey = "AqzV1M_9YQDSZOG7dw03eFj6m_gWikdq_71LU-zleTYWmaWInfYyS8JUZMu4Dfz-";
-var viewer = new Cesium.Viewer('cesiumContainer',{timeline : false, infoBox : false, animation:false , selectionIndicator : false});
+var viewer = new Cesium.Viewer('cesiumContainer',{
+	timeline : false, 
+	infoBox : false, 
+	animation:false , 
+	selectionIndicator : false,
+	homeButton : false,
+	fullscreenButton : false,
+	navigationHelpButton : false,
+	baseLayerPicker : false,
+	geocoder : false,
+	sceneModePicker : false});
 var currentTask = task3;
 viewer.canvas.addEventListener('click',currentTask, false);
 
